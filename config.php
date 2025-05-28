89713a643a21a8ba0608eca4eb996430cec091dc
Stayshy <127775822+Stayshy@users.noreply.github.com>
2025-05-28 08:58:33 +0300

    Что-то

--- config.php
+++
@@ -1,3 +0,0 @@
-<?php
-define('INTELLIGENCE_API_KEY', 'io-v2-eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJvd25lciI6IjI2M2ZjOWY4LTMwZDQtNDkwNS05MmIwLThiYzIxOWNmMDdhMyIsImV4cCI6NDkwMDg2NzA3MX0.afjlYEdO3qhyAJvXIoGsysPgnp8OjHvDX9FyGgKi-Th33lGPo5hQJpK2LG-Y6ZVlp9-VFSVF1wV610zMMI172A'); // Replace with the key that worked in Postman
-?>
